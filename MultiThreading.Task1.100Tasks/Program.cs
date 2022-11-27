8ff8c6cf1099be72ad7c6ab26ffb534c613051a8
srdrflk <55311271+srdrflk@users.noreply.github.com>
2022-11-27 16:49:40 +0300

    Task1.100Tasks completed

--- MultiThreading.Task1.100Tasks/Program.cs
+++ MultiThreading.Task1.100Tasks/Program.cs
@@ -1,9 +1,13 @@
 ﻿/*
- * 1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.
+ * 1. Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.
  * Each Task should iterate from 1 to 1000 and print into the console the following string:
  * “Task #0 – {iteration number}”.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task1._100Tasks
 {
@@ -11,15 +15,8 @@ namespace MultiThreading.Task1._100Tasks
     {
         const int TaskAmount = 100;
         const int MaxIterationsCount = 1000;
-
         static void Main(string[] args)
         {
-            Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
-            Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
-            Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
-            Console.WriteLine("“Task #0 – {iteration number}”.");
-            Console.WriteLine();
-            
             HundredTasks();
 
             Console.ReadLine();
@@ -27,7 +24,18 @@ namespace MultiThreading.Task1._100Tasks
 
         static void HundredTasks()
         {
-            // feel free to add your code here
+            List<Task> taskList = new List<Task>();
+
+            for (int i = 1; i <= TaskAmount; i++)
+            {
+                int k = i;
+                for (int j = 1; j <= MaxIterationsCount; j++)
+                {
+                    int z = j;
+                    taskList.Add(Task.Run(() => Output(k, z)));
+                    Task.WaitAll(taskList.ToArray());
+                }
+            }
         }
 
         static void Output(int taskNumber, int iterationNumber)
