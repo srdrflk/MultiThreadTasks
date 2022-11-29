fc483c8dd5b3c8fd71172bfe172da862f8e60f9a
srdrflk <55311271+srdrflk@users.noreply.github.com>
2022-11-29 16:44:48 +0300

    MultiThreading.Task2.Chaining completed

--- MultiThreading.Task2.Chaining/Program.cs
+++ MultiThreading.Task2.Chaining/Program.cs
@@ -6,6 +6,9 @@
  * Fourth Task – calculates the average value. All this tasks should print the values to console.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task2.Chaining
 {
@@ -13,17 +16,48 @@ namespace MultiThreading.Task2.Chaining
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(".Net Mentoring Program. MultiThreading V1 ");
-            Console.WriteLine("2.	Write a program, which creates a chain of four Tasks.");
-            Console.WriteLine("First Task – creates an array of 10 random integer.");
-            Console.WriteLine("Second Task – multiplies this array with another random integer.");
-            Console.WriteLine("Third Task – sorts this array by ascending.");
-            Console.WriteLine("Fourth Task – calculates the average value. All this tasks should print the values to console");
-            Console.WriteLine();
 
-            // feel free to add your code
+            Task<int[]> taskA = Task.Run(() => CreateIntArray());
+
+            taskA.ContinueWith(arr => MultiplyArray(arr.Result))
+                .ContinueWith(arr => SortArray(arr.Result))
+                .ContinueWith(arr => Console.WriteLine("Average is : " + CalculateAverage(arr.Result)));
 
             Console.ReadLine();
+        }
+
+        public static int[] CreateIntArray()
+        {
+            Random random = new Random();
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(int.MinValue, int.MaxValue);
+            }
+            return array;
+        }
+
+        public static int[] MultiplyArray(int[] array)
+        {
+            int[] newArray = CreateIntArray();
+            array = array.Concat(newArray).ToArray();
+            return array;
+        }
+
+        public static int[] SortArray(int[] array)
+        {
+            var sortedArray = array.OrderBy(x => x).ToArray();
+            return sortedArray;
+        }
+
+        public static int CalculateAverage(int[] array)
+        {
+            int total = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                total += array[i];
+            }
+            return total / array.Length;
         }
     }
 }
